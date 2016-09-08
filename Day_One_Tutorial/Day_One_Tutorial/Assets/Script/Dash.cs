88e913b78fc3ce51fe1f3e22b4e3d701bc6e6fbe
Aaron Freedman <aaron@secretcrush.net>
2016-09-08 10:58:40 -0400

    Day one tutorial project

---
+++ Day_One_Tutorial/Day_One_Tutorial/Assets/Script/Dash.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("Dash"))
+            transform.Translate(new Vector3(-1f, 0, 0));
+        if (Input.GetButtonDown("DashRight"))
+            transform.Translate(new Vector3(1f, 0, 0));
+        
+	
+	}
+}
---
+++ Day_One_Tutorial/Day_One_Tutorial/Assets/Script/Gravity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+    [Range(-1, 1f)]
+    public float GravityStrength;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, GravityStrength, 0));
+	}
+}
