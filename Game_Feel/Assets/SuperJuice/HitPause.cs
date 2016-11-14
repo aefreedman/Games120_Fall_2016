09a4ea9c4632f66e3debd0112a3ca7b3fee38de1
Unknown <je1067@nyu.edu>
2016-11-14 11:25:30 -0500

    Hit Pause Script
    template script for a basic hit pause system

---
+++ Game_Feel/Assets/SuperJuice/HitPause.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitPause : MonoBehaviour {
+
+	public bool hitPause;
+	public float stopTime;
+	public float originalTimeScale;
+
+
+
+	void Start () {
+		originalTimeScale = Time.timeScale;
+		hitPause = false;
+		//MODIFY THIS FOR YOUR NEEDS
+		//stopTime = .5f; 
+	}
+	
+
+	void Update () {
+		//CREATE PARAMETERS FOR HITPAUSE = TRUE
+		//if (Input.GetKeyDown("a")) {
+		//	hitPause = true;
+		//}
+		if (hitPause) {
+			StartCoroutine ("Pause");
+			hitPause = false;
+		}
+	}
+
+
+
+	IEnumerator Pause () {
+		//STOPS TIME
+		Time.timeScale = 0;
+
+		//WAITS FOR STOPTIME SECONDS BEFORE CONTINUING
+		yield return new WaitForSecondsRealtime(stopTime);
+
+		//RESETS TIME BACK TO WHAT IT WAS AT START
+		Time.timeScale = originalTimeScale;
+		
+	}
+
+
+}
+
+
+
