3a58d8a99e98c3631a529aabd5bdb2dafa533f18
Unknown <srl447@nyu.edu>
2016-11-14 11:44:59 -0500

    Made Decals Work

---
+++ Decals/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    Vector3 move;
+	// Use this for initialization
+	void Start () {
+        move = new Vector3(-.09f, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey("left"))
+        {
+            transform.position = transform.position + move;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.position = transform.position - move;
+        }
+        if(Input.GetKey("up"))
+        {
+            transform.position = transform.position + Vector3.up*.1f;
+        }
+
+    }
+}
