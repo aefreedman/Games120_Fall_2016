8867f88b9327718d7416fd4dd3d681f31d5fbeef
Unknown <tcf234@nyu.edu>
2016-11-14 12:14:56 -0500

    damage flash

---
+++ Game_Feel/Assets/Script/DamageFlash.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageFlash : MonoBehaviour {
+
+    public SpriteRenderer render;
+    Color initialColor;
+
+
+    // Use this for initialization
+    void Start () {
+
+        initialColor = render.color;
+        //playerColor = render.color;
+        //redValue = playerColor.r;
+        //greenValue = playerColor.g;
+        //blueValue = playerColor.b;
+        //alphaValue = playerColor.a;
+        Debug.Log(render.color);
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        render.color = initialColor;
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //if (col.gameObject.tag == "thing")
+        //{
+            render.color = new Color(1f, 0, 0);
+            Debug.Log(render.color);
+        //}
+    }
+
+    //void OnCollisionExit2D(Collision2D col)
+    //{
+
+    //    redValue = initialColor.r;
+    //    greenValue = initialColor.g;
+    //    blueValue = initialColor.b;
+
+    //}
+}
